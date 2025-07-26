f6753524882d09f8885687a6a545f2e66f77211d
neel0086 <neelmehta0086@gmail.com>
2025-07-26 17:55:32 +0530

    kafka config

--- URLRedirection/src/main/java/com/service/URLRedirection/config/KafkaProducerConfig.java
+++ URLRedirection/src/main/java/com/service/URLRedirection/config/KafkaProducerConfig.java
@@ -16,7 +16,7 @@ public class KafkaProducerConfig {
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
 
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // or read from application.yml
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092"); // or read from application.yml
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
