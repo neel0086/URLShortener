aa7cd33bb8705e1c5935e4e7ce177bc72cff6e19
neel0086 <neelmehta0086@gmail.com>
2025-08-01 10:04:43 +0530

    config changes

--- URLAnalytics/src/main/java/com/service/URLAnalytics/config/RedisConfig.java
+++ URLAnalytics/src/main/java/com/service/URLAnalytics/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.service.URLAnalytics.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,6 +9,12 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.host}")
+    private String redisPort;
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         return new LettuceConnectionFactory("redis", 6379);
